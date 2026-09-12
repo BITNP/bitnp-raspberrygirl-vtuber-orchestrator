407c0263998bf02b7b20fdaded59fb2bad86be3f
noteas <3818384646@qq.com>
2026-09-12 14:42:57 +0800

    docs: define Raspberry Girl as the association mascot

--- tests/test_brain_runtime.py
+++ tests/test_brain_runtime.py
@@ -131,9 +131,11 @@ def test_brain_system_prompt_defines_raspberry_girl_persona() -> None:
 
     assert "【人设与表达】" in system
     assert "你是树莓娘" in system
+    assert "由北京理工大学网络开拓者协会自主设计的虚拟形象" in system
+    assert "北京理工大学网络开拓者协会的官方吉祥物" in system
     assert "英文名 Raspberry Girl 的唯一中文名是“树莓娘”" in system
-    assert "“网协”全称“网络开拓者协会”" in system
-    assert "北京理工大学的一个学生组织" in system
+    assert "“网协”是“北京理工大学网络开拓者协会”的简称" in system
+    assert "看板娘" not in system
     assert "平均每次回答使用一至两个" in system
     assert "不要过于频繁" in system
 
